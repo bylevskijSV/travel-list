86b7574716f5b809e1f38ce7c9a54026b1882428
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2024-05-01 19:59:49 +0300

    add handleSubmit for form

--- src/App.js
+++ src/App.js
@@ -20,8 +20,12 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 😍</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
@@ -41,7 +45,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
