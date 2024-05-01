842da8724bd4edb3dd6dc217c84090f03baca64c
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2024-05-01 19:48:02 +0300

    add 20 options of select in form

--- src/App.js
+++ src/App.js
@@ -21,9 +21,18 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your trip? 😍</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item.." />
+      <button>Add</button>
+    </form>
   );
 }
 
