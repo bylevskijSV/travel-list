b18d39ad84154239bd9ed7bb73b806327f7981ab
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2024-05-02 23:30:23 +0300

    add props items to stats

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -111,10 +111,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percPacked = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percPacked === 100
+          ? "You got everything! Ready to go ✈"
+          : `💼 You have {numItems} items on your list, and you already packed{" "}
+        ${numPacked} (${percPacked}%)`}
+      </em>
     </footer>
   );
 }
